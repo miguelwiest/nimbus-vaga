7e6fb07a8591cb76bbf69297a3df26eb30039736
Miguel Wiest <wiestmiguel@gmail.com>
2023-11-24 20:08:22 -0300

    refact: .env configuration

--- src/shared/services/index.ts
+++ src/shared/services/index.ts
@@ -1,63 +1,66 @@
 import axios from "axios";
 import {IArea, IPerimeter, IPoint} from "../types";
 
+const BASE_URL = import.meta.env.VITE_API_URL
+
+console.log(BASE_URL)
 export async function getPoints() {
     const response = await axios.get(`http://localhost:3333/points`);
     return response.data;
 }
 
 export async function postPoint(point: IPoint) {
-    const response = await axios.post(`http://localhost:3333/points`, point);
+    const response = await axios.post(`${BASE_URL}/points`, point);
     return response.data;
 }
 
 export async function patchPoint(point: IPoint) {
-    const response = await axios.patch(`http://localhost:3333/points/${point.id}`, point);
+    const response = await axios.patch(`${BASE_URL}/points/${point.id}`, point);
     return response.data;
 }
 
 export async function deletePoint(id: string) {
-    const response = await axios.delete(`http://localhost:3333/points/${id}`);
+    const response = await axios.delete(`${BASE_URL}/points/${id}`);
     return response.data;
 }
 
 export async function getAreas() {
-    const response = await axios.get(`http://localhost:3333/areas`);
+    const response = await axios.get(`${BASE_URL}/areas`);
     return response.data;
 }
 
 export async function postArea(area: IArea) {
     console.log(area.id)
-    const response = await axios.post(`http://localhost:3333/areas`, area);
+    const response = await axios.post(`${BASE_URL}/areas`, area);
     return response.data;
 }
 
 export async function patchArea(area: IArea) {
-    const response = await axios.patch(`http://localhost:3333/areas/${area.id}`, area);
+    const response = await axios.patch(`${BASE_URL}/areas/${area.id}`, area);
     return response.data;
 }
 
 export async function deleteArea(id: string) {
-    const response = await axios.delete(`http://localhost:3333/areas/${id}`);
+    const response = await axios.delete(`${BASE_URL}/areas/${id}`);
     return response.data;
 }
 
 export async function getPerimeters() {
-    const response = await axios.get(`http://localhost:3333/perimeters`);
+    const response = await axios.get(`${BASE_URL}/perimeters`);
     return response.data;
 }
 
 export async function postPerimeter(perimeter: IPerimeter) {
-    const response = await axios.post(`http://localhost:3333/perimeters`, perimeter);
+    const response = await axios.post(`${BASE_URL}/perimeters`, perimeter);
     return response.data;
 }
 
 export async function patchPerimeter(perimeter: IPerimeter) {
-    const response = await axios.patch(`http://localhost:3333/perimeters/${perimeter.id}`, perimeter);
+    const response = await axios.patch(`${BASE_URL}/perimeters/${perimeter.id}`, perimeter);
     return response.data;
 }
 
 export async function deletePerimeter(id: string) {
-    const response = await axios.delete(`http://localhost:3333/perimeters/${id}`);
+    const response = await axios.delete(`${BASE_URL}/perimeters/${id}`);
     return response.data;
 }
